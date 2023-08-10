28489213563ab361c906a11b8efed0ea1b89fd40
shancakes <sdonnelly91420@gmail.com>
2023-08-10 01:29:21 -0400

    edited number display

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
 const bingoNumbers = Array.from({ length: 25 }, (_, i) => i + 1);
 const freeSpace = "Free Space";
 const shuffledNumbers = shuffle(bingoNumbers);
 const boardContainer = document.getElementById('board');
-const drawnNumber = document.getElementById('drawn-number');
+const drawnNumberDiv = document.getElementById('drawn-number'); // Updated variable name
 
+// Creating the board
 function createBoard() {
     boardContainer.innerHTML = '';
 
@@ -17,6 +17,7 @@ function createBoard() {
     }
 }
 
+// Resetting the board
 function resetBoard() {
     const reshuffledNumbers = shuffle(bingoNumbers);
     shuffledNumbers.length = 0;
@@ -24,6 +25,7 @@ function resetBoard() {
     createBoard();
 }
 
+// Shuffling the numbers
 function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -32,12 +34,15 @@ function shuffle(arr) {
     return arr;
 }
 
+// Drawing a number
 function drawNumber() {
     const drawnNumber = Math.floor(Math.random() * 25) + 1;
-    drawnNumber.textContent = `Number drawn: ${drawnNumber}`;
+    drawnNumberDiv.textContent = `Number drawn: ${drawnNumber}`;
     highlightSquare(drawnNumber);
+    checkForWin();
 }
 
+// Highlighting the square red
 function highlightSquare(number) {
     const boardItems = document.querySelectorAll('.board-item');
     for (let i = 0; i < boardItems.length; i++) {
@@ -49,29 +54,27 @@ function highlightSquare(number) {
 
 //all winning bingo line combinations
 const bingoWinningLines = [
+    // Horizontal lines
     [0, 1, 2, 3, 4],
     [5, 6, 7, 8, 9],
     [10, 11, 12, 13, 14],
     [15, 16, 17, 18, 19],
     [20, 21, 22, 23, 24],
+    // Vertical lines
     [0, 5, 10, 15, 20],
     [1, 6, 11, 16, 21],
     [2, 7, 12, 17, 22],
     [3, 8, 13, 18, 23],
     [4, 9, 14, 19, 24],
+    // Diagonal lines
     [0, 6, 12, 18, 24],
     [4, 8, 12, 16, 20],
-    [0, 1, 2, 3, 4, 12],
-    [0, 5, 10, 15, 20, 12],
-    [20, 21, 22, 23, 24, 12],
-    [4, 9, 14, 19, 24, 12],
-    [0, 6, 12, 18, 24, 0],
-    [4, 8, 12, 16, 20, 4]
+
+
 ];
 
 
-
-
+// Check for a win
 function checkForWin() {
     const boardItems = document.querySelectorAll('.board-item');
 
@@ -82,34 +85,14 @@ function checkForWin() {
             // Highlight the winning squares in yellow
             for (const index of line) {
                 boardItems[index].classList.add('winning-square');
-                boardItems[index].style.backgroundColor = 'yellow'; // Highlight in yellow
+                boardItems[index].style.backgroundColor = 'yellow';
             }
             // Display the winning message and reload the page on closing the alert
             alert("BINGO! YOU WON!");
-
-            break;
+            break; // Stop the loop
         }
     }
 }
 
-// Modal functions
-function showModal() {
-    const modal = document.getElementById('bingo-modal');
-    modal.style.display = 'block';
-}
-
-//Closing the modal refreshes the page
-function closeModal() {
-    location.reload();
-}
-
-function drawNumber() {
-    const drawnNumber = Math.floor(Math.random() * 25) + 1;
-    drawnNumber.textContent = `Number drawn: ${drawnNumber}`;
-    highlightSquare(drawnNumber);
-    checkForWin();
-}
-
-
-
+// Initialize the board
 createBoard();
